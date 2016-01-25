89b7661aea67fc7095a3c3904cd23d1d314aa1f3
liumeng <liushuijiwusi@163.com>
2016-01-25 16:23:52 +0800

    地图改进

--- src/main/webapp/META-INF/script/map.js
+++ src/main/webapp/META-INF/script/map.js
@@ -7,6 +7,7 @@
 	};
 	var map;
 	var point;
+	 var mapClusterer;
 	var myIcon = new BMap.Icon("http://api.map.baidu.com/img/markers.png", new BMap.Size(23, 25), {
 		 offset: new BMap.Size(10, 25), // 指定定位位置
 		 imageOffset: new BMap.Size(0, 0 - 10 * 25) // 设置图片偏移
@@ -18,6 +19,7 @@
 		map.enableScrollWheelZoom();	
 		var bottom_right_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_BOTTOM_RIGHT,offset:0});	
 		map.addControl(bottom_right_control);//添加地图比例尺
+		mapClusterer = new BMapLib.MarkerClusterer(map,{maxZoom:13,isAverangeCenter:true});
 		getData();
 	//	var marker = new BMap.Marker(new BMap.Point(120.15755,33.36664));  // 创建标注
 		//map.addOverlay(marker);     
@@ -31,15 +33,30 @@
 			   };
 	function addClickHandler(content,marker){
 		marker.addEventListener("click",function(e){
-			openInfo(content,e)}
+			var infoWindow = new BMapLib.SearchInfoWindow(map,content,
+				    {
+				//    title: title, //标题
+						width: 230, //宽度
+						height: 180, //高度
+						panel : "panel", //检索结果面板
+						enableAutoPan : true, //自动平移
+						enableSendToPhone: true, //是否显示发送到手机按钮
+						searchTypes :[
+							BMAPLIB_TAB_SEARCH,   //周边检索
+							BMAPLIB_TAB_TO_HERE,  //到这里去
+							BMAPLIB_TAB_FROM_HERE //从这里出发
+						]
+						}
+					);
+			var position=new BMap.Point(e.target.getPosition().lng,e.target.getPosition().lat);
+			infoWindow.open(position); 
+			}
 		);
 	}
 	
 	var optionListener=function(){
 		$('#socia_cont .option').on('click',$(this),function(){
 			 var choice = $(this).html();
-	//		 point = new BMap.Point(120.15755, 33.35);
-	//			map.centerAndZoom(point, 15);
 			 var local = new BMap.LocalSearch(map, {
 					renderOptions:{map: map}
 					});
@@ -47,18 +64,12 @@
 		});
 	}
 	
-	function openInfo(content,e){
-		var p = e.target;
-		var point = new BMap.Point(p.getPosition().lng, p.getPosition().lat);
-		var infoWindow = new BMap.InfoWindow(content,opts);  // 创建信息窗口对象 
-		map.openInfoWindow(infoWindow,point); //开启信息窗口
-	} 
 	var showparks=function(){
 		
 		for(var i=0;i<data_info.length;i++){
 			var marker = new BMap.Marker(new BMap.Point(data_info[i][0],data_info[i][1]),{icon: myIcon});  // 创建标注
 			var content = data_info[i][2];
-			map.addOverlay(marker);               // 将标注添加到地图中
+			mapClusterer.addMarker(marker);
 			addClickHandler(content,marker);
 		}
 	}
@@ -89,12 +100,10 @@
 				getPoints(parkdata);
 			},
 		});
-	}
-	
+	}	
     function getPoints(parkdata) {
     	var v_html='';
-        for (var i=0;i<parkdata.length;i++) {
-        	
+        for (var i=0;i<parkdata.length;i++) {      	
             v_html += "<li p_id=" + i + ">";
             v_html += '<h1 class="font18 color_3">' + parkdata[i].name + '</h1>';
             v_html += '<p>空余车位：<b class="green">' + parkdata[i].portLeftCount + '</b></p>';
@@ -103,19 +112,14 @@
                 v_html += '<a href="#" class="res back_orange font18 radius_3 absolute reservation" pid="' + i + '"><i class="i"></i>预定</a>';
             v_html += '</li>';
         }
-        //setpage(page, maxPage);
         $("#parking_list").html(v_html);
         $("#parking_list li").hover(function () {
             var p_id = $(this).attr('p_id');
-            $('#parking_rm_' + p_id).removeClass('map_p_btn').addClass('map_p_btn_on');
-            
+            $('#parking_rm_' + p_id).removeClass('map_p_btn').addClass('map_p_btn_on');          
         }, function () {
             var p_id = $(this).attr('p_id');
             $('#parking_rm_' + p_id).removeClass('map_p_btn_on').addClass('map_p_btn');
         })
     }
-	
-	function convertData(){
-		
-	}
+
 })(jQuery);
